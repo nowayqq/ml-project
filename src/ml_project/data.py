15f5c216aa751c32c69682e22936484c39c15a6c
nowayqq <set.mr@inbox.ru>
2022-05-10 11:41:40 +1000

    report creation moved to get_data function

--- src/ml_project/data.py
+++ src/ml_project/data.py
@@ -7,8 +7,14 @@ import click
 import pandas as pd
 
 
+def generate_report(csv_path: Path):
+    return ProfileReport(pd.read_csv(
+        csv_path)).to_file("../../EDA_report.html")
+
+
 def get_data(
-        csv_path: Path, random_state: int, test_split_ratio: float
+        csv_path: Path, random_state: int, test_split_ratio: float,
+        gen_rep=False
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     data = pd.read_csv(csv_path)
     click.echo(f"Dataset shape: {data.shape}.")
@@ -17,9 +23,6 @@ def get_data(
     features_train, features_val, target_train, target_val = train_test_split(
         features, target, test_size=test_split_ratio, random_state=random_state
     )
+    if gen_rep:
+        generate_report(csv_path)
     return features_train, features_val, target_train, target_val
-
-
-def generate_report(csv_path: Path):
-    return ProfileReport(pd.read_csv(
-        csv_path)).to_file("../../EDA_report.html")
