ba968d8642b18d761905f7fdada69acae72bbc3e
nowayqq <set.mr@inbox.ru>
2022-05-10 13:37:13 +1000

    Add CLI options for get_data() function

--- src/ml_project/train.py
+++ src/ml_project/train.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 import click
-import pandas as pd
+
+from .data import get_data
 
 
 @click.command()
@@ -11,8 +12,33 @@ import pandas as pd
     default='../../data/train.csv',
     type=click.Path(exists=True, dir_okay=False, path_type=Path)
 )
+@click.option(
+    '--random-state',
+    default=42,
+    type=int,
+    show_default=True
+)
+@click.option(
+    '--test-split-ratio',
+    default=0.2,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+    show_default=True
+)
+@click.option(
+    '--gen_rep',
+    default=False,
+    type=bool,
+    show_default=True
+)
 def train(
-        dataset_path: Path
+        dataset_path: Path,
+        random_state: int,
+        test_split_ratio: float,
+        gen_rep: bool,
 ) -> None:
-    dataset = pd.read_csv(dataset_path)
-    click.echo(f'Dataset shape: {dataset.shape}.')
+    features_train, features_val, target_train, target_val = get_data(
+        dataset_path,
+        random_state,
+        test_split_ratio,
+        gen_rep
+    )
